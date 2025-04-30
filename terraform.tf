b3ab3c93297c7f5b6e41e7282265f1f1f33ea341
prashant-aggarwal <prashant.aggarwal05@gmail.com>
2025-04-30 19:17:59 +0000

    commit

--- terraform.tf
+++ terraform.tf
@@ -2,14 +2,15 @@
 # SPDX-License-Identifier: MPL-2.0
 
 terraform {
-
+  /*
   cloud {
-    organization = "organization-name"
+    organization = "HCP-Getting-Started"
 
     workspaces {
       name = "learn-terraform"
     }
   }
+  */
 
   required_providers {
     aws = {
